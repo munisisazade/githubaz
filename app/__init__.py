75202f7a6350c0ec3dc7b333e596e312f68b3583
Munis İsazadə <m.isazade@asan.gov.az>
2019-02-28 18:14:10 +0400

    Latest changes

--- app/__init__.py
+++ app/__init__.py
@@ -9,14 +9,22 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile(os.path.join(BASE_DIR, 'instance/config.py'))
 
-    from flask import render_template, jsonify, request
+    from flask import render_template, jsonify, request, session
     import time
+    from datetime import datetime
     import subprocess
 
     @app.route("/", methods=["GET"])
     def index_page():
-        context = {"data": [1,2,3,4]}
+        context = {"now": datetime.now().strftime("%Y")}
         return render_template("home/index.html", **context)
+
+    @app.route("/api", methods=["GET"])
+    def egov_bridge():
+        print(session)
+        return jsonify({
+            "service": "Egov.bridge"
+        })
 
     @app.route("/execute", methods=["POST"])
     def run_code_snipped():
@@ -37,16 +45,21 @@ except:
         cmd = subprocess.Popen(".venv/bin/python " + compiler_file,shell=True,
                                stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         result = cmd.stdout.read().decode("utf-8")
-        print("Birinci ele => ",result.split("\n")[0])
-        if len(result.split("LINE:")) > 1:
-            line = result.split("LINE:")[1].split("\n")[0]
-        else:
-            line = result.split("LINE:")[0].split("\n")[0]
-        line = int(line) - 3 if line != "" else None
         os.remove(compiler_file)
-        return jsonify({
-            "result": result.split("ERROR:")[1].replace("line {}".format(line+2),"line {}".format(line)),
-            "line": line
-        })
+        if not "LINE:" in result:
+            return jsonify({
+                "result": result.replace("\n", "<br>"),
+                "line": None
+            })
+        else:
+            if len(result.split("LINE:")) > 1:
+                line = result.split("LINE:")[1].split("\n")[0]
+            else:
+                line = result.split("LINE:")[0].split("\n")[0]
+            line = int(line) - 3 if line != "" else None
+            return jsonify({
+                "result": result.split("ERROR:")[1],
+                "line": line
+            })
 
     return app
--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -15,7 +15,7 @@ $(function() {
         });
 
 
-        editor.getSession().on('change', function() {
+       /* editor.getSession().on('change', function() {
             var code = editor.getValue();
             $.ajax({
                 url: "/execute",
@@ -44,7 +44,7 @@ $(function() {
                     console.log(xhr, err, traceback);
                 }
             })
-        });
+        });*/
 
         ace.config.loadModule("ace/ext/emmet", function() {
             ace.require("ace/lib/net").loadScript("https://cloud9ide.github.io/emmet-core/emmet.js", function() {
@@ -71,138 +71,37 @@ $(function() {
           </div>');
         console.log("error");
     }
-    $("ul.menu-list").mousedown(function(e) {
-        if (e.button === 1) {
-            e.preventDefault();
-        }
+
+    $(".execute-button").click(function (e) {
+       e.preventDefault();
+       var code = editor.getValue();
+            $.ajax({
+                url: "/execute",
+                type: "POST",
+                data: code,
+                processData: false,
+                contentType: false,
+                success: function (response) {
+                    var result_code = $(".code-result");
+                    result_code.html(response.result.replace("\n","<br>"));
+                    if (response.line) {
+                        editor.getSession().setAnnotations([{
+                          row: response.line,
+                          column: 0,
+                          text: response.result, // Or the Json reply from the parser
+                          type: "error" // also warning and information
+                        }]);
+                        result_code.css("color", "red");
+                    }
+                    else {
+                        editor.getSession().clearAnnotations();
+                        result_code.css("color", "#555");
+                    }
+                },
+                error: function (xhr, err, traceback) {
+                    console.log(xhr, err, traceback);
+                }
+            })
     });
 
-    $("ul.menu-list li").on("click auxclick", function(e) {
-        if (e.target.tagName !== "A") {
-            var href = $(this).find("a").attr("href");
-            if (e.originalEvent.button == 1)
-                window.open(href, "_blank");
-            else if (e.originalEvent.button == 0)
-                window.location = href;
-        }
-    });
-
-    // used when page is access directly
-    function magicClickInterceptor(e) {
-        e.preventDefault();
-
-        var state = {};
-        state.api = $(this).attr("href").substring(6, $(this).attr("href").length - 5);
-        $.bbq.pushState(state);
-
-        var _self = $(this);
-        $("#apiHolder").load($(this).attr("href") + " #documentation", function(){
-        $("#apiHolder").removeClass("apiIntro").removeClass("span9");
-        $("#documentation").removeClass("span9").addClass("span7");
-            ux();
-            setupClicker();
-
-            // handles dropping in from new link
-            var section = $.bbq.getState("section");
-            if (section) {
-                $("li#dropdown_" + section.replace(/\./g, '\\.') + " a").triggerHandler('click');
-            }
-
-            //setupDisqus(_self.attr("href"));
-        });
-    }
-
-    $('.menu-item a').click(magicClickInterceptor);
-    $('a.argument').click(magicClickInterceptor);
-
-    $('a.external').click(function(e) {
-        e.preventDefault();
-    });
-
-    var tabs = $("#tabnav"),
-        tab_a_selector = "a";
-
-    var firstLoad = true;
-
-    tabs.find(tab_a_selector).click(function(e) {
-        e.preventDefault();
-        if ($(this).attr("href") === "/") {
-            window.location = "http://ace.ajax.org";
-            return;
-        }
-        if ($(this).attr("href").indexOf("#api") === 0) {
-            $("#top_container").addClass("collapse");
-            scrollIntoPosition(null, 0);
-        }
-        else if ($(this).is(":visible")) {
-            if (firstLoad) {
-                firstLoad = false;
-                setTimeout(function() {
-                    $("#top_container").removeClass("collapse");
-                    scrollIntoPosition(e.target);
-                }, 700);
-            }
-            else {
-                $("#top_container").removeClass("collapse");
-                scrollIntoPosition(e.target);
-            }
-        }
-
-        function scrollIntoPosition(el, overridePosition) {
-            if (typeof overridePosition !== "undefined") {
-                $("body").stop().animate({
-                    scrollTop: overridePosition
-                }, 400);
-            }
-            else if ($("body").scrollTop() > 345) {
-                $("body").stop().animate({
-                    scrollTop: ($(el).offset().top - 10)
-                }, 400);
-            }
-        }
-
-        $($(this).attr("href") + " img[lazy-src]").each(function() {
-              var val = $.attr(this, "lazy-src");
-              $.attr(this, "src", val);
-              $.removeAttr(this, "lazy-src");
-        });
-
-        $(this).tab("show");
-
-        var state = {};
-        state.nav = $(this).attr("href").substr(1);
-        var oldState = $.bbq.getState("nav") || "about";
-        if (state.nav != oldState)
-            $.bbq.pushState(state);
-     });
-
-    $('#tabnav a[data-toggle="tab"]').on('shown', function (e) {
-        $(".tab-content .tab-pane.active .ace_editor").each(function(i, el){
-            el.env.onResize();
-        });
-    });
-
-
-    highlight();
 });
-
-
-
-function highlight() {
-    var highlighter = ace.require("ace/ext/static_highlight")
-    var dom = ace.require("ace/lib/dom")
-    function qsa(sel) {
-        var els = document.querySelectorAll(sel);
-        var result = [];
-        for (var i = 0, l = els.length; i < l; i++)
-            result[i] = els[i];
-        return result;
-    }
-
-    qsa("code[class]").forEach(function(el) {
-        var m = el.className.match(/language-(\w+)|(javascript)/);
-        if (!m) return
-        var mode = "ace/mode/" + (m[1] || m[2]);
-        highlighter.highlight(el, {mode: mode, theme: "ace/theme/xcode"})
-    });
-}
